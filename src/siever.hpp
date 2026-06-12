111354f785765069f3ac5e494f06d9c77742d2a4
Youcef L <youcefl@users.noreply.github.com>
2026-06-12 19:42:52 +0100

    Make it possible to use LFP for sieving.

--- src/siever.hpp
+++ src/siever.hpp
@@ -10,7 +10,8 @@
 #include <primesieve.hpp>
 #endif
 #ifdef CUTRIALDIVE_HAS_LFP
-//#include <lfp.hpp>
+#include <lfp.hpp>
+#include <omp.h>
 #endif
 
 #if !defined CUTRIALDIVE_HAS_PRIMESIEVE && !defined CUTRIALDIVE_HAS_LFP
@@ -19,29 +20,32 @@
 
 namespace cutrialdive {
 
-    enum Siever {
+    enum sieve_lib
+    {
         primesieve,
         lfp
     };
 
-    template <uint32_t siever>
+    template <uint32_t SieveLib>
     void sievex(uint64_t start, uint64_t end, std::vector<uint64_t> & primes)
     {
-        static_assert(siever == Siever::primesieve
-            || siever == Siever::lfp);
+        static_assert((SieveLib == sieve_lib::primesieve)
+                     || (SieveLib == sieve_lib::lfp));
 
-        if constexpr (siever == Siever::primesieve) {
+        if constexpr (SieveLib == sieve_lib::primesieve) {
 #ifdef CUTRIALDIVE_HAS_PRIMESIEVE
             primesieve::generate_primes(start, end ? end - 1 : end, &primes);
 #else
-            static_assert(false, "Sieving library primesieve is not available");
+            static_assert(SieveLib != sieve_lib::primesieve, "Sieving library primesieve is not available");
 #endif
         }
-        else if constexpr(siever == Siever::lfp) {
+        else if constexpr(SieveLib == sieve_lib::lfp) {
 #ifdef CUTRIALDIVE_HAS_LFP
-            lfp::sieve_into_vector<uint64_t>(start, end, primes);
+            auto sieveRes = lfp::sieve<uint64_t>(start, end, lfp::threads{uint32_t(omp_get_max_threads())});
+            auto rng = sieveRes.range();
+            primes.assign(rng.begin(), rng.end());
 #else
-            //static_assert(false, "Sieving library lfp is not available");
+            static_assert(SieveLib != sieve_lib::lfp, "Sieving library lfp is not available");
 #endif
         }
     }
@@ -49,12 +53,12 @@ namespace cutrialdive {
 #ifdef CUTRIALDIVE_HAS_PRIMESIEVE
     inline void sieve(uint64_t start, uint64_t end, std::vector<uint64_t> & primes)
     {
-        sievex<Siever::primesieve>(start, end, primes);
+        sievex<sieve_lib::primesieve>(start, end, primes);
     }
 #elif defined CUTRIALDIVE_HAS_LFP 
-    inline std::vector<uint64_t> sieve(uint64_t start, uint64_t end)
+    inline void sieve(uint64_t start, uint64_t end, std::vector<uint64_t> & primes)
     {
-        sievex<Siever::primesieve>(start, end, primes);
+        sievex<sieve_lib::lfp>(start, end, primes);
     }
 #endif
 }
